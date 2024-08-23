b74c5e9140acb189adea1ff44823568fb88a33e3
Rani-Chandrasekaran <rani.chandrasekaran@gmail.com>
2024-08-23 14:46:23 +0530

    done

---
+++ interview prparation/findlargestnum.py
@@ -0,0 +1,14 @@
+'''
+ *Find the Largest Element*
+   - *Description*: Write a function that takes a list of numbers and returns the largest number.
+   - *Input*: [3, 1, 4, 1, 5, 9]
+   - *Output*: 9
+   - *Input*: [-10, -20, -30]
+   - *Output*: -10
+   '''
+num = [3, 1, 4, 1, 5, 9]
+num1 = [-10, -20, -30]
+maxnum = max(num)
+maxnum1 = max(num1)
+print(f"Largest Element in the list {num} is {maxnum}")
+print(f"Largest Element in the list {num1} is {maxnum1}")
