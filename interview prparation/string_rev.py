06108ccdc4a39c7ab9cbf76d174fca04a73fa962
Rani-Chandrasekaran <rani.chandrasekaran@gmail.com>
2024-08-23 11:45:00 +0530

    start

---
+++ interview prparation/string_rev.py
@@ -0,0 +1,9 @@
+'''
+1. *Reverse a String*
+   - *Description*: Write a function that takes a string and returns it reversed.
+   - *Input*: "hello"
+   - *Output*: "olleh"
+'''
+word_input = "hello"
+word_output = word_input[::-1]
+print("Reverse a String is:" ,word_output)
